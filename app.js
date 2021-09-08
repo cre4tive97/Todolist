c8fca82ac25b3a356581eed0b08742ff3c37532f
harryborrison5148 <harryborrison5148@gmail.com>
2021-09-08 21:03:15 +0900

    JS watch

---
+++ app.js
@@ -0,0 +1,29 @@
+function watch() {
+  const date = new Date();
+  const dateName = ["months", "days", "hours", "minutes", "seconds"];
+  const dateFunction = [
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+  ];
+
+  const [months, days, hours, minutes, seconds] = dateFunction;
+  const dateArray = [months, days, hours, minutes, seconds];
+
+  const dateNumber = document.querySelectorAll(".date__num");
+  const dateTitle = document.querySelectorAll(".date__title");
+
+  for (let i = 0; i < dateNumber.length; i++) {
+    dateNumber[i].innerHTML = dateArray[i];
+    dateTitle[i].innerHTML = dateName[i];
+  }
+}
+
+function init() {
+  watch();
+  setInterval(watch, 1000);
+}
+
+init();
